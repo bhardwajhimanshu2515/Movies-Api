cb97af0a7b29aa43d55da239da2adaf0dba463ce
bhardwajhimanshu2515@gmail.com <bhardwajhimanshu2515@gmail.com>
2020-08-21 20:00:33 +0530

    himanshu | created server and routes are set

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+
+
+const usersRoutes = require('./src/routes/users.routes');
+
+const app = express();
+
+//usages
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+
+
+// TOKEN AUTHENTICATION- ALL THE ROUTES WRITTEN BELOW THIS WILL NEED TOKEN TO BE SENT in request headers
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+  next();
+});
+
+//using routes
+app.use('/api/users', usersRoutes);
+
+// heroku
+var dburl = "mongodb://127.0.0.1:27017/credencedb";
+
+mongoose
+  .connect(dburl,{ useUnifiedTopology: true, 
+
+      useNewUrlParser:true, useCreateIndex: true,  useFindAndModify: false })
+  .then(() => {
+    console.log("Server has started.......")
+    app.listen(process.env.PORT || 5000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
