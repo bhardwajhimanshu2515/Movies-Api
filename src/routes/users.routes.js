d0155b8dd2b5c898179b4b5b0c07713e30f951fc
bhardwajhimanshu2515@gmail.com <bhardwajhimanshu2515@gmail.com>
2020-08-21 20:05:23 +0530

    himanshu | users.routes.js created

---
+++ src/routes/users.routes.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const checkToken = require('../middlewares/checkToken')
+const { check } = require('express-validator');
+
+const usersController = require("../controllers/users.controller");
+
+
+const router = express.Router();
+
+router.post('/signup',  
+[
+    check('name')
+      .not()
+      .isEmpty(),
+    check('email')
+      .normalizeEmail() // Test@test.com => test@test.com
+      .isEmail(),
+    check('password').isLength({ min: 6 })
+  ],
+  usersController.singin);
+
+router.post('/login', usersController.login);
+
+//all the api written below this will require a token
+router.use(checkToken);
+
+router.get('/', usersController.getUsers);
+router.get('/specificUser', usersController.getspecificUser);
+
+
+module.exports = router;
+
+
